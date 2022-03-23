9ca20288fe29334a88bb674016547bce8e33d3d2
jsteyn135 <78400904+jsteyn135@users.noreply.github.com>
2022-03-23 00:46:47 -0600

    figuring out rtc lib
    rtc lib is a library to use with the ds1307-Module-V03 real time clock module(elegoo)

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Smart-Alarm-Clock
- 
+
+
+
+## install
+
+### https://www.arduino.cc/reference/en/libraries/rtclib/
+## https://adafruit.github.io/RTClib/html/class_date_time.html
+### ---------------------------------------------------
+### 
--- smart_alarm/smart_alarm/smart_alarm.ino
+++ smart_alarm/smart_alarm/smart_alarm.ino
@@ -1,11 +1,34 @@
 // Ridhisha Dangol and Jacques Steyn
+#include <RTClib.h>
+
+RTC_DS1307 rtc;
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600);
+  rtc.begin();
+
+  if(! rtc.isrunning()){
+    rtc.adjust(DateTime(2022,3,23,12,30,0));
+    
+  }
+  rtc.adjust(DateTime(2022,3,23,12,30,0));
   
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  DateTime current = rtc.now();
+  Serial.print(current.year());
+  Serial.print('/');
+  Serial.print(current.month());
+  Serial.print('/');
+  Serial.print(current.day());
+  Serial.print("\n");
+  Serial.print(current.hour());
+  Serial.print(':');
+  Serial.print(current.minute());
+  Serial.print(':');
+  Serial.print(current.second());
+  Serial.println();
+  Serial.println();
+  delay(10000);
 }
